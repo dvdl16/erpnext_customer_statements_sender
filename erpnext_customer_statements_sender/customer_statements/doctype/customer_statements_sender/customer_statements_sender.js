806140129dcb7fce47b607ad3a839a79c0ab215e
Dirk van der Laarse <dirk@laarse.co.za>
2020-02-13 21:54:22 +0200

    Added doctypes and structure

---
+++ erpnext_customer_statements_sender/customer_statements/doctype/customer_statements_sender/customer_statements_sender.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2020, Dirk van der Laarse and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Customer Statements Sender', {
+	// refresh: function(frm) {
+
+	// }
+});
