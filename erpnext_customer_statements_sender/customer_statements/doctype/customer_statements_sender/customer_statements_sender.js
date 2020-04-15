c7338715eab752069172edb6dd27e8a38e5f16d2
Dirk van der Laarse <dirk@laarse.co.za>
2020-04-15 23:08:07 +0200

    Remove Add Row button

--- erpnext_customer_statements_sender/customer_statements/doctype/customer_statements_sender/customer_statements_sender.js
+++ erpnext_customer_statements_sender/customer_statements/doctype/customer_statements_sender/customer_statements_sender.js
@@ -2,6 +2,10 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Customer Statements Sender', {
+  refresh: function(frm) {
+    // Hide Add Row button
+    $('div[id="page-Form/Customer Statements Sender"] div.small.form-clickable-section.grid-footer').css({'display': 'none'});
+  },
 	get_customer_emails: function(frm) {
     frappe.call({
       method: "populate_recipient_list",
