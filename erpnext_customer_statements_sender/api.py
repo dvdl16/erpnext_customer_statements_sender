73ded91706418232256ee01db0e7afe8d488df08
Dirk van der Laarse <dirk@laarse.co.za>
2020-04-16 21:07:00 +0200

    Testing basic functionality

--- erpnext_customer_statements_sender/api.py
+++ erpnext_customer_statements_sender/api.py
@@ -1,87 +1,107 @@
-# from __future__ import unicode_literals
-# import frappe
-# from frappe.model.document import Document
-# from frappe.utils import flt, time_diff_in_hours, get_datetime, getdate, today, cint, add_days, get_link_to_form
-# from frappe import _
-# from frappe.utils.xlsxutils import make_xlsx
-# import json
-#
-#
-# @frappe.whitelist()
-# def send_statements():
-#
-# 	email_list = frappe.db.get_values('Send Customer Statements Customers', {'parent': 'Send Customer Statements'}, ['customer_name','email','send_statement'])
-# 	for customer_name, email, send_statement in email_list:
-# 		if email is not None:
-# 			if send_statement == "Yes":
-# 				data = get_report_content(customer_name)
-# 				if not data:
-# 					return
-#
-# 				attachments = [{
-# 					'fname': get_file_name(),
-# 					'fcontent': data
-# 				}]
-#
-# 				frappe.sendmail(
-# 					recipients = email,
-# 					subject = "Customer Statement from Simply Garlic",
-# 					message = "Good day. <br> Please find attached your latest statement from Simply Garlic",
-# 					attachments = attachments,
-# 					reference_doctype = "Report",
-# 					reference_name="General Ledger"
-# 				)
-#
-# 	frappe.msgprint('Emails queued for sending')
-#
-# def get_report_content(customer_name):
-# 	'''Returns file in for the report in given format'''
-#
-# 	# Borrowed code from frappe/email/doctype/auto_email_report/auto_email_report.py
-# 	report = frappe.get_doc('Report', "General Ledger")
-# 	custom_filter = {'company': "Horizon Global SA (Pty) Ltd", 'party_type': "Customer", 'party': customer_name, 'from_date': add_days(today(), -7),'to_date': today(), 'group_by': "Group by Voucher (Consolidated)"}
-#
-# 	columns, data = report.get_data(limit=100 or 100, user = "Administrator", filters = custom_filter, as_dict=True)
-#
-# 	# add serial numbers
-# 	columns.insert(0, frappe._dict(fieldname='idx', label='', width='30px'))
-# 	for i in range(len(data)):
-# 		data[i]['idx'] = i+1
-#
-# 	# For .xlsx
-# 	spreadsheet_data = get_spreadsheet_data(columns, data)
-# 	xlsx_file = make_xlsx(spreadsheet_data, "Auto Email Report")
-# 	return xlsx_file.getvalue()
-#
-# 	# For .html
-# 	# columns, data = make_links(columns, data)
-# 	# return self.get_html_table(columns, data)  <-------------Fails here....... see auto_email_report.py
-#
-#
-# def get_spreadsheet_data(columns, data):
-# 	out = [[_(df.label) for df in columns], ]
-# 	for row in data:
-# 		new_row = []
-# 		out.append(new_row)
-# 		for df in columns:
-# 			if df.fieldname not in row: continue
-# 			new_row.append(frappe.format(row[df.fieldname], df, row))
-#
-# 	return out
-#
-# def get_file_name():
-# 	return "{0}.{1}".format("Customer Statement".replace(" ", "-").replace("/", "-"), "xlsx")
-# 	# return "{0}.{1}".format("Customer Statement".replace(" ", "-").replace("/", "-"), "html")
-#
-#
-# # def make_links(columns, data):
-# # 	for row in data:
-# # 		for col in columns:
-# # 			if col.fieldtype == "Link" and col.options != "Currency":
-# # 				if col.options and row.get(col.fieldname):
-# # 					row[col.fieldname] = get_link_to_form(col.options, row[col.fieldname])
-# # 			elif col.fieldtype == "Dynamic Link":
-# # 				if col.options and row.get(col.fieldname) and row.get(col.options):
-# # 					row[col.fieldname] = get_link_to_form(row[col.options], row[col.fieldname])
-# #
-# # 	return columns, data
+from __future__ import unicode_literals
+import frappe
+from frappe.model.document import Document
+from frappe.utils import flt, time_diff_in_hours, get_datetime, getdate, today, cint, add_days, get_link_to_form, format_time, global_date_format, now, get_url_to_report
+from frappe.utils.xlsxutils import make_xlsx
+from frappe.utils.pdf import get_pdf
+from frappe import _
+import json
+from pprint import pprint
+
+@frappe.whitelist()
+def send_statements(options=None):
+
+	email_list = get_recipient_list()
+	for row in email_list:
+		print('Checking customer', row.customer)
+		if row.email_id is not None:
+			if row.send_statement == "Yes":
+				print('Need to send statement to customer',row.customer)
+				data = get_report_content(row.customer)
+				if not data:
+					return
+
+				attachments = [{
+					'fname': get_file_name(),
+					'fcontent': data
+				}]
+
+				frappe.sendmail(
+					recipients = row.email_id,
+					subject = "Customer Statement from Simply Garlic",
+					message = "Good day. <br> Please find attached your latest statement from Simply Garlic",
+					attachments = attachments,
+					reference_doctype = "Report",
+					reference_name="General Ledger"
+				)
+
+	frappe.msgprint('Emails queued for sending')
+
+@frappe.whitelist()
+def get_report_content(customer_name):
+	'''Returns file in for the report in given format'''
+
+	# Borrowed code from frappe/email/doctype/auto_email_report/auto_email_report.py
+	report = frappe.get_doc('Report', "General Ledger")
+
+	custom_filter = {
+					'company': 'Golden Mole',
+					'party_type': 'Customer',
+					'party': [customer_name],
+					'from_date': add_days(today(), -90),
+					'to_date': today(),
+					'group_by': 'Group by Voucher (Consolidated)'}
+
+	columns, data = report.get_data(limit=500, user = "Administrator", filters = custom_filter, as_dict=True)
+
+	# add serial numbers
+	columns.insert(0, frappe._dict(fieldname='idx', label='', width='30px'))
+	for i in range(len(data)):
+		data[i]['idx'] = i+1
+
+	date_time = global_date_format(now()) + ' ' + format_time(now())
+	report_html_data = frappe.render_template('erpnext_customer_statements_sender/templates/report/customer_statement_jinja.html', {
+		'title': 'Customer Statement for X',
+		'description': 'Customer Statement for X',
+		'date_time': date_time,
+		'columns': columns,
+		'data': data,
+		'report_name': 'Customer Statement for X',
+		'filters': custom_filter
+	})
+
+	print(report_html_data)
+
+	pdf_data = get_pdf(report_html_data)
+	return pdf_data
+
+
+def get_file_name():
+	return "{0}.{1}".format("Customer Statement".replace(" ", "-").replace("/", "-"), "pdf")
+	# return "{0}.{1}".format("Customer Statement".replace(" ", "-").replace("/", "-"), "html")
+
+
+
+
+
+@frappe.whitelist()
+def get_recipient_list():
+	return frappe.db.sql("""SELECT
+								customer,
+								contact,
+								email_id,
+								MIN(priority) AS priority,
+								send_statement
+							FROM
+								(SELECT
+									tab_cus.name AS 'customer',
+									tab_con.name AS 'contact',
+									tab_con.email_id,
+									CASE WHEN is_customer_statement_contact = 1 THEN 1 WHEN tab_con.is_primary_contact = 1 THEN 2 ELSE 3 END AS 'priority',
+									CASE WHEN tab_cus.disable_customer_statements = 1 THEN 'No (Disabled for this customer)' WHEN ISNULL(tab_con.email_id) OR tab_con.email_id = '' THEN 'No (No email address on record)' ELSE 'Yes' END AS 'send_statement'
+								FROM `tabCustomer` AS tab_cus
+									LEFT JOIN `tabDynamic Link` as tab_dyn ON tab_dyn.link_name = tab_cus.name AND tab_dyn.link_doctype = 'Customer'
+									LEFT JOIN `tabContact` as tab_con ON tab_dyn.parent = tab_con.name AND tab_dyn.parenttype = 'Contact'
+								WHERE tab_cus.disabled = 0) AS t_contacts
+							GROUP BY customer
+							ORDER BY customer""", as_dict=True)
--- erpnext_customer_statements_sender/customer_statements/doctype/customer_statements_sender/customer_statements_sender.py
+++ erpnext_customer_statements_sender/customer_statements/doctype/customer_statements_sender/customer_statements_sender.py
@@ -5,6 +5,7 @@
 from __future__ import unicode_literals
 import frappe
 from frappe.model.document import Document
+from erpnext_customer_statements_sender.api import get_recipient_list
 
 class CustomerStatementsSender(Document):
 	def populate_recipient_list(self):
@@ -17,25 +18,3 @@ class CustomerStatementsSender(Document):
 			row.contact = c.contact
 			row.email = c.email_id
 			row.send_statement = c.send_statement
-
-@frappe.whitelist()
-def get_recipient_list():
-	return frappe.db.sql("""SELECT
-								customer,
-								contact,
-								email_id,
-								MIN(priority) AS priority,
-								send_statement
-							FROM
-								(SELECT
-									tab_cus.name AS 'customer',
-									tab_con.name AS 'contact',
-									tab_con.email_id,
-									CASE WHEN is_customer_statement_contact = 1 THEN 1 WHEN tab_con.is_primary_contact = 1 THEN 2 ELSE 3 END AS 'priority',
-									CASE WHEN tab_cus.disable_customer_statements = 1 THEN 'No (Disabled for this customer)' WHEN ISNULL(tab_con.email_id) OR tab_con.email_id = '' THEN 'No (No email address on record)' ELSE 'Yes' END AS 'send_statement'
-								FROM `tabCustomer` AS tab_cus
-									LEFT JOIN `tabDynamic Link` as tab_dyn ON tab_dyn.link_name = tab_cus.name AND tab_dyn.link_doctype = 'Customer'
-									LEFT JOIN `tabContact` as tab_con ON tab_dyn.parent = tab_con.name AND tab_dyn.parenttype = 'Contact'
-								WHERE tab_cus.disabled = 0) AS t_contacts
-							GROUP BY customer
-							ORDER BY customer""", as_dict=True)
